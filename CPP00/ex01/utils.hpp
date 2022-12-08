ad9f39c98806f861c9747051467dea528dff34d0
e-zaki <85259775+e-zaki@users.noreply.github.com>
2022-12-08 03:49:59 +0100

    Delete CPP00 directory

--- CPP00/ex01/utils.hpp
+++
@@ -1,9 +0,0 @@
-
-// prototypes of additional used functs:
-
-int				getIndex(int nbr_contacts);
-std::string		getPhoneNum(std::string msg);
-std::string		getName(std::string msg);
-bool 			OnlyDigits(std::string s);
-bool			printable(std::string s);
-bool			emptyEntry(std::string s);
